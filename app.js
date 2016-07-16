d8a1a0c9753d1e19ef7088aab870c44574e52bbf
Alborz G <aghalamk@gmail.com>
2016-07-15 17:25:20 -0700

    Added increment & decrement counters

--- app.js
+++ app.js
@@ -2,12 +2,37 @@ angular.module('tasty', []);
 angular.module('tasty')
 .controller('MainController', function($scope, $http) {
 	$scope.title = "Tasty"
-	$scope.counter = 0;
-	var count = $scope.counter;
+	$scope.counter = 1;
+    console.log($scope.counter);
+
+    $scope.increment = function(counter){
+        $scope.counter += 1;
+         //$scope.updateGif();
+				 scope.$apply();   
+         console.log($scope.counter);
+
+      }
+    $scope.decrement = function(counter){
+        if(counter<=0)
+            {
+                return;
+            }
+
+        $scope.counter -= 1;
+         console.log($scope.counter);
+
+      }
+
+    $scope.updateGif = function(counter) {
+      $http.get("https://www.reddit.com/r/gifrecipes.json")
+    .then(function(response) {
+        $scope.gifData = response.data.data.children[$scope.counter].data.preview.images[0].variants.gif.source.url;
+    });
+    }
 	// Add capability to increment and decrement between gifs and pages with counters
 	//https://www.reddit.com/r/gifrecipes#page=1
     $http.get("https://www.reddit.com/r/gifrecipes.json")
     .then(function(response) {
-        $scope.gifData = response.data.data.children[count].data.preview.images[0].variants.gif.source.url;
+        $scope.gifData = response.data.data.children[$scope.counter].data.preview.images[0].variants.gif.source.url;
     });
 });
